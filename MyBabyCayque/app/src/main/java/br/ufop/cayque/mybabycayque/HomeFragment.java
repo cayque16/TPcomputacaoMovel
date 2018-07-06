e492175ec926647808b530041383c1c29dbdc0fe
cayque16 <cayque_santos16@hotmail.com>
2018-07-06 18:04:52 -0300

    Adaptações
    - ao clicar na lista da home, agora é direcionado a tela de edit;
    - especificações do trabalho alteradas;
    - novo menu adicionado: som de útero.

--- MyBabyCayque/app/src/main/java/br/ufop/cayque/mybabycayque/HomeFragment.java
+++ MyBabyCayque/app/src/main/java/br/ufop/cayque/mybabycayque/HomeFragment.java
@@ -11,6 +11,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -41,6 +42,12 @@ import br.ufop.cayque.mybabycayque.add.AddMedicamentosActivity;
 import br.ufop.cayque.mybabycayque.add.AddOutrosActivity;
 import br.ufop.cayque.mybabycayque.add.AddSonecaActivity;
 import br.ufop.cayque.mybabycayque.controllers.HistoricoSingleton;
+import br.ufop.cayque.mybabycayque.edit.EditFraldasActivity;
+import br.ufop.cayque.mybabycayque.edit.EditMamadasActivity;
+import br.ufop.cayque.mybabycayque.edit.EditMamadeirasActivity;
+import br.ufop.cayque.mybabycayque.edit.EditMedicamentosActivity;
+import br.ufop.cayque.mybabycayque.edit.EditOutrosActivity;
+import br.ufop.cayque.mybabycayque.edit.EditSonecasActivity;
 import br.ufop.cayque.mybabycayque.models.Atividades;
 import br.ufop.cayque.mybabycayque.models.Fraldas;
 import br.ufop.cayque.mybabycayque.models.GeraIdSingleton;
@@ -71,8 +78,8 @@ public class HomeFragment extends Fragment {
     private Boolean checkT = false;
     private int spinnerIdx = 0;
     private TextView textVazia;
-    private FloatingActionButton fabMamada,fabMamadeira,fabFralda,fabSoneca;
-    private FloatingActionButton fabMedicamento,fabOutro;
+    private FloatingActionButton fabMamada, fabMamadeira, fabFralda, fabSoneca;
+    private FloatingActionButton fabMedicamento, fabOutro;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -118,105 +125,69 @@ public class HomeFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 String tipo = HistoricoSingleton.getInstance().getAtividades().get(i).getTipo();
-                TextView dataI, dataT, horaI, horaT, peito, quanti;
-                TextView anotacao, tudo, motivo, nome;
+                int id = HistoricoSingleton.getInstance().getAtividades().get(i).getId();
+                int sizeArray;
 
                 if (tipo.equals("Mamada")) {
-                    Mamadas mamada = (Mamadas) HistoricoSingleton.getInstance().getAtividades().get(i);
-                    if (dialog != null) {
-                        dialog.setContentView(R.layout.dialog_mamada);
-                        dataI = dialog.findViewById(R.id.dialogMamadaDataI);
-                        dataI.setText(conversor(mamada.getDiaInicio()) + "/" + conversor(mamada.getMesInico()) + "/" + conversor(mamada.getAnoInicio()));
-                        horaI = dialog.findViewById(R.id.dialogMamadaHoraI);
-                        horaI.setText(conversor(mamada.getHoraInicio()) + ":" + conversor(mamada.getMinuInicio()) + ":" + conversor(mamada.getSeguInicio()));
-                        horaT = dialog.findViewById(R.id.dialogMamadaHoraT);
-                        horaT.setText(conversor(mamada.getHoraTermino()) + ":" + conversor(mamada.getMinuTermino()) + ":" + conversor(mamada.getSeguTermino()));
-                        peito = dialog.findViewById(R.id.dialogMamadaPeito);
-                        peito.setText(mamada.getPeito());
-                        anotacao = dialog.findViewById(R.id.dialogMamadaAnotacao);
-                        anotacao.setText(mamada.getAnotacao());
-                        dialog.show();
-                    }
-                } else if (tipo.equals("Mamadeira")) {
-                    Mamadeiras mamadeira = (Mamadeiras) HistoricoSingleton.getInstance().getAtividades().get(i);
-                    if (dialog != null) {
-                        dialog.setContentView(R.layout.dialog_mamadeira);
-                        dataI = dialog.findViewById(R.id.dialogMamadeiraDataI);
-                        dataI.setText(conversor(mamadeira.getDiaInicio()) + "/" + conversor(mamadeira.getMesInico()) + "/" + conversor(mamadeira.getAnoInicio()));
-                        horaI = dialog.findViewById(R.id.dialogMamadeiraHoraI);
-                        horaI.setText(conversor(mamadeira.getHoraInicio()) + ":" + conversor(mamadeira.getMinuInicio()) + ":" + conversor(mamadeira.getSeguInicio()));
-                        horaT = dialog.findViewById(R.id.dialogMamadeiraHoraT);
-                        horaT.setText(conversor(mamadeira.getHoraTermino()) + ":" + conversor(mamadeira.getMinuTermino()) + ":" + conversor(mamadeira.getSeguTermino()));
-                        quanti = dialog.findViewById(R.id.dialogMamadeiraQuanti);
-                        quanti.setText(mamadeira.getQuantidade() + "ml");
-                        tudo = dialog.findViewById(R.id.dialogMamadeiraTomou);
-                        if (mamadeira.getTomouTudo() == 1) {
-                            tudo.setText("Sim");
-                        } else {
-                            tudo.setText("Não");
+                    sizeArray = HistoricoSingleton.getInstance().getMamadas().size();
+                    Intent it = new Intent(getContext(), EditMamadasActivity.class);
+                    for (int j = 0; j < sizeArray; j++) {
+                        if (HistoricoSingleton.getInstance().getMamadas().get(j).getId() == id) {
+                            it.putExtra("position", j);
+                            break;
                         }
-                        anotacao = dialog.findViewById(R.id.dialogMamadeiraAnotacao);
-                        anotacao.setText(mamadeira.getAnotacao());
-                        dialog.show();
                     }
+                    startActivity(it);
+                } else if (tipo.equals("Mamadeira")) {
+                    sizeArray = HistoricoSingleton.getInstance().getMamadeiras().size();
+                    Intent it = new Intent(getContext(), EditMamadeirasActivity.class);
+                    for (int j = 0; j < sizeArray; j++) {
+                        if (HistoricoSingleton.getInstance().getMamadeiras().get(j).getId() == id) {
+                            it.putExtra("position", j);
+                            break;
+                        }
+                    }
+                    startActivity(it);
                 } else if (tipo.equals("Fralda")) {
-                    Fraldas fralda = (Fraldas) HistoricoSingleton.getInstance().getAtividades().get(i);
-                    if (dialog != null) {
-                        dialog.setContentView(R.layout.dialog_fralda);
-                        dataI = dialog.findViewById(R.id.dialogFraldaDataI);
-                        dataI.setText(conversor(fralda.getDiaInicio()) + "/" + conversor(fralda.getMesInico()) + "/" + conversor(fralda.getAnoInicio()));
-                        horaI = dialog.findViewById(R.id.dialogFraldaHoraI);
-                        horaI.setText(conversor(fralda.getHoraInicio()) + ":" + conversor(fralda.getMinuInicio()) + ":" + conversor(fralda.getSeguInicio()));
-                        motivo = dialog.findViewById(R.id.dialogFraldaMotivo);
-                        motivo.setText(fralda.getMotivo());
-                        anotacao = dialog.findViewById(R.id.dialogFraldaAnotacao);
-                        anotacao.setText(fralda.getAnotacao());
-                        dialog.show();
+                    sizeArray = HistoricoSingleton.getInstance().getFraldas().size();
+                    Intent it = new Intent(getContext(), EditFraldasActivity.class);
+                    for (int j = 0; j < sizeArray; j++) {
+                        if (HistoricoSingleton.getInstance().getFraldas().get(j).getId() == id) {
+                            it.putExtra("position", j);
+                            break;
+                        }
                     }
+                    startActivity(it);
                 } else if (tipo.equals("Soneca")) {
-                    Sonecas soneca = (Sonecas) HistoricoSingleton.getInstance().getAtividades().get(i);
-                    if (dialog != null) {
-                        dialog.setContentView(R.layout.dialog_soneca);
-                        dataI = dialog.findViewById(R.id.dialogSonecaDataI);
-                        dataI.setText(conversor(soneca.getDiaInicio()) + "/" + conversor(soneca.getMesInico()) + "/" + conversor(soneca.getAnoInicio()));
-                        dataT = dialog.findViewById(R.id.dialogSonecaDataT);
-                        dataT.setText(conversor(soneca.getDiaTermino()) + "/" + conversor(soneca.getMesTermino()) + "/" + conversor(soneca.getAnoTermino()));
-                        horaI = dialog.findViewById(R.id.dialogSonecaHoraI);
-                        horaI.setText(conversor(soneca.getHoraInicio()) + ":" + conversor(soneca.getMinuInicio()) + ":" + conversor(soneca.getSeguInicio()));
-                        horaT = dialog.findViewById(R.id.dialogSonecaHoraT);
-                        horaT.setText(conversor(soneca.getHoraTermino()) + ":" + conversor(soneca.getMinuTermino()) + ":" + conversor(soneca.getSeguTermino()));
-                        anotacao = dialog.findViewById(R.id.dialogSonecaAnotacao);
-                        anotacao.setText(soneca.getAnotacao());
-                        dialog.show();
+                    sizeArray = HistoricoSingleton.getInstance().getSonecas().size();
+                    Intent it = new Intent(getContext(), EditSonecasActivity.class);
+                    for (int j = 0; j < sizeArray; j++) {
+                        if (HistoricoSingleton.getInstance().getSonecas().get(j).getId() == id) {
+                            it.putExtra("position", j);
+                            break;
+                        }
                     }
+                    startActivity(it);
                 } else if (tipo.equals("Medicamento")) {
-                    Medicamentos medicamento = (Medicamentos) HistoricoSingleton.getInstance().getAtividades().get(i);
-                    if (dialog != null) {
-                        dialog.setContentView(R.layout.dialog_medicamento);
-                        dataI = dialog.findViewById(R.id.dialogMedicamentoDataI);
-                        dataI.setText(conversor(medicamento.getDiaInicio()) + "/" + conversor(medicamento.getMesInico()) + "/" + conversor(medicamento.getAnoInicio()));
-                        horaI = dialog.findViewById(R.id.dialogMedicamentoHoraI);
-                        horaI.setText(conversor(medicamento.getHoraInicio()) + ":" + conversor(medicamento.getMinuInicio()) + ":" + conversor(medicamento.getSeguInicio()));
-                        nome = dialog.findViewById(R.id.dialogMedicamentoNome);
-                        nome.setText(medicamento.getNome());
-                        quanti = dialog.findViewById(R.id.dialogMedicamentoQuanti);
-                        quanti.setText(medicamento.getDose() + " " + medicamento.getUnidadeMedi() + "(s)");
-                        anotacao = dialog.findViewById(R.id.dialogMedicamentoAnotacao);
-                        anotacao.setText(medicamento.getAnotacao());
-                        dialog.show();
+                    sizeArray = HistoricoSingleton.getInstance().getMedicamentos().size();
+                    Intent it = new Intent(getContext(), EditMedicamentosActivity.class);
+                    for (int j = 0; j < sizeArray; j++) {
+                        if (HistoricoSingleton.getInstance().getMedicamentos().get(j).getId() == id) {
+                            it.putExtra("position", j);
+                            break;
+                        }
                     }
+                    startActivity(it);
                 } else if (tipo.equals("Outro")) {
-                    Outros outro = (Outros) HistoricoSingleton.getInstance().getAtividades().get(i);
-                    if (dialog != null) {
-                        dialog.setContentView(R.layout.dialog_outro);
-                        dataI = dialog.findViewById(R.id.dialogOutroDataI);
-                        dataI.setText(conversor(outro.getDiaInicio()) + "/" + conversor(outro.getMesInico()) + "/" + conversor(outro.getAnoInicio()));
-                        horaI = dialog.findViewById(R.id.dialogOutroHoraI);
-                        horaI.setText(conversor(outro.getHoraInicio()) + ":" + conversor(outro.getMinuInicio()) + ":" + conversor(outro.getSeguInicio()));
-                        anotacao = dialog.findViewById(R.id.dialogOutroAnotacao);
-                        anotacao.setText(outro.getAnotacao());
-                        dialog.show();
+                    sizeArray = HistoricoSingleton.getInstance().getOutros().size();
+                    Intent it = new Intent(getContext(), EditOutrosActivity.class);
+                    for (int j = 0; j < sizeArray; j++) {
+                        if (HistoricoSingleton.getInstance().getOutros().get(j).getId() == id) {
+                            it.putExtra("position", j);
+                            break;
+                        }
                     }
+                    startActivity(it);
                 }
             }
         });
@@ -477,7 +448,6 @@ public class HomeFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        //para atualizar o ListView quando voltar da tela de Add
         listView.setAdapter(new AtividadesAdapter(HistoricoSingleton.getInstance().getAtividades(), getContext()));
     }
 
